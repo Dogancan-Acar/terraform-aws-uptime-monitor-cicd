c45d18233c294d4aa3bfe7dd89bb9a99cd69124f
Dogancan <d.can0@hotmail.com>
2026-03-02 16:20:49 +0300

    fix: match resource names in main and outputs

--- output.tf
+++ output.tf
@@ -1,9 +1,9 @@
 output "instance_public_ip" {
   description = "The public IP address of the EC2 instance"
-  value       = aws_instance.uptime_server.public_ip
+  value       = aws_instance.uptime-server.public_ip
 }
 
 output "api_url" {
   description = "The URL to access the API documentation"
-  value       = "http://${aws_instance.uptime_server.public_ip}:8080/docs"
+  value       = "http://${aws_instance.uptime-server.public_ip}:8080/docs"
 }
